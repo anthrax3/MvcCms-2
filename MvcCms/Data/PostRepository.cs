9914dabe2111706ba5fbed377b2d650ab2ad43f2
enrique7mc <enrique7mc@gmail.com>
2015-05-13 21:04:27 -0500

    Adds empty implementation of IPostRepository

---
+++ MvcCms/Data/PostRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvcCms.Models;
+
+namespace MvcCms.Data
+{
+    public class PostRepository : IPostRepository
+    {
+        public Post Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(string id, Post updatedItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Post model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
