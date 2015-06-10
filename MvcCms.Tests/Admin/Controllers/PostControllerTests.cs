eb6926dedeef0b200561dda14ab5e2f2c9cb441f
enrique7mc <enrique7mc@gmail.com>
2015-06-09 19:51:56 -0500

    Fixes the posts tests

--- MvcCms.Tests/Admin/Controllers/PostControllerTests.cs
+++ MvcCms.Tests/Admin/Controllers/PostControllerTests.cs
@@ -6,6 +6,7 @@ using MvcCms.Areas.Admin.Controllers;
 using MvcCms.Data;
 using MvcCms.Models;
 using Telerik.JustMock;
+using System.Threading.Tasks;
 
 namespace MvcCms.Tests.Admin.Controllers
 {
@@ -14,107 +15,114 @@ namespace MvcCms.Tests.Admin.Controllers
     {        
 
         [TestMethod]
-        public void Edit_GetRequestSendsPostToView()
+        public async Task Edit_GetRequestSendsPostToView()
         {
             var id = "test-post";
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
 
             Mock.Arrange(() => repo.Get(id))
                 .Returns(new Post {Id = id});
 
-            var result = (ViewResult) controller.Edit(id);
+            var result = (ViewResult) await controller.Edit(id);
             var model = (Post) result.Model;
 
             Assert.AreEqual(id, model.Id);
         }
 
         [TestMethod]
-        public void Edit_GetRequestNotFoundResult()
+        public async Task Edit_GetRequestNotFoundResult()
         {
             var id = "test-post";
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
 
             Mock.Arrange(() => repo.Get(id))
                 .Returns((Post)null);
 
-            var result = controller.Edit(id);            
+            var result = await controller.Edit(id);            
 
             Assert.IsTrue(result is HttpNotFoundResult);
         }
 
         [TestMethod]
-        public void Edit_PostRequestNotFoundResult()
+        public async Task Edit_PostRequestNotFoundResult()
         {
             var id = "test-post";
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
             var post = new Post {Id = "Mock Post"};
             Mock.Arrange(() => repo.Edit(id, post))
                 .Throws(new KeyNotFoundException());            
 
-            var result = controller.Edit(id, post);
+            var result = await controller.Edit(id, post);
 
             Assert.IsTrue(result is HttpNotFoundResult);
         }
 
         [TestMethod]
-        public void Edit_PostRequestSendsPostToView()
+        public async Task Edit_PostRequestSendsPostToView()
         {
             var id = "test-post";
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
 
             Mock.Arrange(() => repo.Get(id))
                 .Returns(new Post { Id = id });
 
             controller.ViewData.ModelState.AddModelError("key", "error message");
 
-            var result = (ViewResult)controller.Edit(id, new Post(){Id = "test-post-2"});
+            var result = (ViewResult)await controller.Edit(id, new Post(){Id = "test-post-2"});
             var model = (Post)result.Model;
 
             Assert.AreEqual("test-post-2", model.Id);
         }
 
         [TestMethod]
-        public void Edit_PostRequestCallsEditAndRedirects()
+        public async Task Edit_PostRequestCallsEditAndRedirects()
         {            
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
 
             Mock.Arrange(() => repo.Edit(Arg.IsAny<string>(), Arg.IsAny<Post>()))
                 .MustBeCalled();
 
-            var result = controller.Edit("foo", new Post { Id = "test-post-2" });
+            var result = await controller.Edit("foo", new Post { Id = "test-post-2" });
 
             Mock.Assert(repo);
             Assert.IsTrue(result is RedirectToRouteResult);
         }        
 
         [TestMethod]
-        public void Create_PostRequestErrorModelState()
+        public async Task Create_PostRequestErrorModelState()
         {            
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
             controller.ViewData.ModelState.AddModelError("key", "error message");
 
-            var result = (ViewResult)controller.Create(new Post{Id = "test-id"});
+            var result = (ViewResult)await controller.Create(new Post{Id = "test-id"});
             var model = (Post)result.Model;
 
             Assert.AreEqual("test-id", model.Id);
         }
 
         [TestMethod]
-        public void Create_PostRequestCallsCreateSendPostToView()
+        public async Task Create_PostRequestCallsCreateSendPostToView()
         {
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
 
             Mock.Arrange(() => repo.Create(Arg.IsAny<Post>()))
                 .MustBeCalled();
 
-            var result = controller.Create(new Post { Id = "test-id" });
+            var result = await controller.Create(new Post { Id = "test-id" });
 
             Mock.Assert(repo);
             Assert.IsTrue(result is RedirectToRouteResult);
@@ -124,7 +132,8 @@ namespace MvcCms.Tests.Admin.Controllers
         public void Create_GetRequestSendsModelToView()
         {            
             var repo = Mock.Create<IPostRepository>();
-            var controller = new PostController(repo);            
+            var userRepo = Mock.Create<IUserRepository>();
+            var controller = new PostController(repo, userRepo);
 
             var result = (ViewResult)controller.Create();
             var model = result.Model;
